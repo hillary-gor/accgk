5f474c8c1f5c201ba474ed92315a1e46ac600559
Hillary Gor <125491337+hillary-se@users.noreply.github.com>
2025-03-20 18:55:23 +0300

    add accgk logo, update nav & home

---
+++ app/components/Hero.tsx
@@ -0,0 +1,11 @@
+export function Hero() {
+    return (
+        <div className="pt-4">
+            <div className="px-[20px] text-[32px] text-center font-bold leading-[40px]">
+                <h1>Association of Certified Care Givers Kenya</h1>
+                
+            </div>
+
+        </div>
+    );
+}
