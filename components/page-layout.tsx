b1180acb2961580fd4fd856250869f165b349c37
Hillary Gor <125491337+hillary-se@users.noreply.github.com>
2025-05-16 15:07:14 +0300

    Removed Wave Divider from PageLayout component

--- components/page-layout.tsx
+++ components/page-layout.tsx
@@ -17,7 +17,7 @@ export default function PageLayout({ title, description, children }: PageLayoutP
               <p className="text-xl text-white/90">{description}</p>
             </div>
           </div>
-          {/* Wave Divider */}
+          {/* Wave Divider
           <div className="absolute bottom-0 left-0 right-0">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" className="w-full h-auto">
               <path
@@ -26,7 +26,7 @@ export default function PageLayout({ title, description, children }: PageLayoutP
                 d="M0,64L80,69.3C160,75,320,85,480,80C640,75,800,53,960,48C1120,43,1280,53,1360,58.7L1440,64L1440,120L1360,120C1280,120,1120,120,960,120C800,120,640,120,480,120C320,120,160,120,80,120L0,120Z"
               ></path>
             </svg>
-          </div>
+          </div>*/}
         </section>
 
         <section className="py-16 md:py-24 bg-white">
